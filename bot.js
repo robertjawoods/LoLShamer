1ac1acbac015122cd4781071971221b2f5791fbd
Robert Woods <robert.woods@thecommercepartnership.com>
2020-07-18 22:27:26 +0100

    Changes

--- bot.js
+++ bot.js
@@ -1,54 +1,105 @@
 const eris = require('eris');
 const fs = require('fs');
 const variables = require('./variables.json');
-const summoners = require('./summoners.json');
-const axios = require('axios').default.create({
-        baseURL: variables.riotApiBaseUrl,
-        headers: {
-            'X-Riot-Token': variables.riotApiToken
-        }
-    }
-);
+const summoners = require('./summoners.json').summoners;
+const riotApi = require('./riotApi')
+
 const _ = require('underscore');
 
-const bot = new eris.Client(variables.botToken, {
-    prefix: '!'
+const bot = new eris.CommandClient(variables.botToken, {
+    prefix:'!'
 });
 
-const getSummonerLevel = function(msg) {
-    let summonerName = msg.content.replace('!summoner', '').trimLeft().trimRight(); 
+function isFeeding() {
+    _.forEach(summoners, (summoner) => { 
+        let summonerName = summoner.summonerName;
 
-    let encodedSummonerName = encodeURI(summonerName);
+        riotApi.getSummonerByName(summonerName)
+        .then(res => { 
+            let data = res.data; 
 
-    axios
-    .get(`lol/summoner/v4/summoners/by-name/${encodedSummonerName}`)
-    .then((res) => { 
-        var data = res.data;
-
-        console.log(res);
-        msg.channel.createMessage(`Summoner ${data.name} is level ${data.summonerLevel}... what a scrub.`);
-    })
-    .catch((err) => {
-        if (err.response.status === variables.notFound) {
-            msg.channel.createMessage(`Summoner "${summonerName}" doesn't exist, you fucking moron.`);
-        }
+            return data.accountId;
+        })
+        .then(accountId => {
+            return riotApi.getMatchesByAccountId(accountId);
+        })
+        .then(response => { 
+            let matchList = response.data.matches;
+            
+            let promises = [];
+            
+            _.forEach(matchList, match => promises.push(riotApi.getMatchDetails(match.gameId)))
+            
+        })
+        .catch(err => {
+            console.log(err)
+        });
     });
 }
 
+setInterval(isFeeding, 6000);
+
 bot.on('ready', () => console.log("Ready")); 
-
-bot.on('messageCreate', async (msg) => {
-    if (msg.channel.name !== 'bot_spam')
-        return;
-
-    if (msg.content.includes('!summoner'))
-        getSummonerLevel(msg);
-});
 
 bot.on('error', err => {
     console.warn(err);
 });
 
+bot.registerCommand('!summonerLevel', (msg, args) =>
+{
+    if (!args[0])
+        return;
+
+    riotApi.getSummonerByName(args[0])
+    .then((res) => { 
+        var data = res.data;
+
+        msg.channel.createMessage(`Summoner ${data.name} is level ${data.summonerLevel}... what a scrub.`);
+    })
+    .catch((err) => {
+        if (err.response.status === variables.notFound) {
+            msg.channel.createMessage(`Summoner "${args[0]}" doesn't exist, you fucking moron.`);
+        }
+    });
+});
+
+bot.registerCommand('!addSummoner', async (msg, args) => { 
+    let summonerName = args[0]; 
+    let discordName = args[1];
+
+    if (!(summonerName && discordName) || args.length > 2)    
+    {
+        msg.channel.createMessage('Invalid command format, please use `!addSummoner {summonerName} {discordName}`. Ensure that the discord name is a valid mention.');
+        return;
+    }
+
+    let summonerFound = false;
+    await riotApi.getSummonerByName(summonerName)      
+    .then(() => {
+        summonerFound = true;
+    }) 
+    .catch((error) => {
+        if (error.response.status === variables.notFound) {
+            msg.channel.createMessage(`Summoner "${summonerName}" doesn't exist, you fucking moron.`);
+        }
+    });
+
+    if (!summonerFound)
+        return;
+
+    summoners.push({
+        "summonerName": summonerName, 
+        "discordName": discordName
+    });
+
+    let data = JSON.stringify({"summoners":summoners})
+
+    fs.writeFile('./summoners.json', data, err => { 
+        if (err) 
+            msg.channel.createMessage('Could not add summoner to list, please contact <@123495583397707776> to find out why');
+        else 
+            msg.channel.createMessage(`Summoner "${summonerName}" added.`);
+    })
+});
+
 bot.connect();
-
-
---
+++ riotApi.js
@@ -0,0 +1,28 @@
+const variables = require('./variables.json');
+const axios = require('axios').default.create({
+    baseURL: variables.riotApiBaseUrl,
+    headers: {
+        'X-Riot-Token': variables.riotApiToken
+    }
+});
+
+exports.getSummonerByName = function(summonerName) {
+    let encodedSummonerName = encodeURI(summonerName.trimLeft().trimRight()); 
+
+    return axios.get(`lol/summoner/v4/summoners/by-name/${encodedSummonerName}`);   
+};
+
+exports.getMatchesByAccountId = function(accountId) { 
+    // Get a date 90 mins ago, will get games that ended within that time 
+    let date = new Date(Date.now() - (variables.gamesInTheLastMinutes * 60000)).valueOf();
+
+    return axios.get(`lol/match/v4/matchlists/by-account/${accountId}`, {
+        params: { 
+            beginTime: date
+        }
+    });
+}
+
+exports.getMatchDetails = function(matchId) { 
+    return axios.get(`/lol/match/v4/matches/${matchId}`);
+}
